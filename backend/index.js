abea623b73b2e8ff66c472448b833dc85a49e0d6
MartinAndres97 <83889815+MartinAndres97@users.noreply.github.com>
2021-11-17 15:21:18 -0500

    test get method

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,30 @@
+const express = require('express');
+const { appCheck } = require('firebase-admin');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+
 const db = require('./src/db/crudcalculadora.js');
 
-db.getDatos();
+app.get('/', function(req, res) {
+    res.send('servidor express express')
+});
+app.get('/nueva-ruta', (req, res) => {
+    res.send('this is a new pathway')
+});
+app.get('/get-users/:id', (req, res) => {
+    const eid = req.params.id;
+    res.send('the doc have been found with ' + eid);
+
+})
+app.get('/get-user', (req, res) => {
+    res.json({
+        "name": "carolina",
+        "DNI": 45634
+    })
+
+})
+
+app.listen(port, () => {
+    console.log('my port is listening ' + port);
+});
