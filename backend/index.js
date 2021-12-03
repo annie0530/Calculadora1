c237e66ba52b06b9f4f5ccfbe637e769a3a5d764
jsilva463 <43388184+jsilva463@users.noreply.github.com>
2021-12-02 21:23:31 -0500

    add Heroku Config

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var cors = require('cors');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 8080;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
